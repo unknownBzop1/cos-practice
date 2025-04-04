2b67a1e80b202b4946d2f4cd28a73ec207c5b002
unknownBzop1 <gwondohwi@gmail.com>
2025-04-04 10:08:10 +0900

    250404

---
+++ 5/modulo.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int mod(int a, int b) {
+	return (a >= 0 ? a : -a) % b;
+}
+
+int len(char* string) {
+	int length = 0;
+	for (char* cursor = string; *cursor; cursor++) {
+		length++;
+	}
+
+	return length;
+}
+
+int convertToInt(const char *snum) {
+	int absoluteValue = 0;
+	int sign = 1;
+
+	if (*snum == '-') {
+		sign = -1;
+		snum++;
+	}
+
+	for (const char* cursor = snum; *cursor; cursor++) {
+		absoluteValue *= 10;
+		absoluteValue += (*cursor - '0');
+	}
+
+	return sign * absoluteValue;
+}
+
+int main(int argc, const char * argv[]) {
+	if (argc != 3) {
+		cout << "Please insert two numbers" << endl;
+		return 1;
+	}
+
+	int a = convertToInt(argv[1]);
+	int b = convertToInt(argv[2]);
+	cout << a << " % " << b << " = " << mod(a, b) << endl;
+
+	return 0;
+}
